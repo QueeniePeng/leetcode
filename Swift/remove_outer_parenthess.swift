bdf451c988b5c26344f5020e0189488c1853f1aa
Queenie Peng <pengqueenie@gmail.com>
2023-01-25 22:12:37 -0600

    remove outer parenthess and divisor game

---
+++ Swift/remove_outer_parenthess.swift
@@ -0,0 +1,60 @@
+// A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+
+// For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+// A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B nonempty valid parentheses strings.
+
+// Given a valid parentheses string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses strings.
+
+// Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
+
+ 
+
+// Example 1:
+
+// Input: s = "(()())(())"
+// Output: "()()()"
+// Explanation: 
+// The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+// After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+// Example 2:
+
+// Input: s = "(()())(())(()(()))"
+// Output: "()()()()(())"
+// Explanation: 
+// The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+// After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+// Example 3:
+
+// Input: s = "()()"
+// Output: ""
+// Explanation: 
+// The input string is "()()", with primitive decomposition "()" + "()".
+// After removing outer parentheses of each part, this is "" + "" = "".
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s[i] is either '(' or ')'.
+// s is a valid parentheses string.
+
+class Solution {
+    func removeOuterParentheses(_ s: String) -> String {
+        var d: [Character:Int] = [:]
+        var start = 0
+        var ans = ""
+
+        for (i, c) in s.enumerated() {
+            d[c, default: 0] += 1
+
+            if d["("] == d[")"] {
+              ans += s[s.index(s.startIndex, offsetBy: start + 1)..<s.index(s.startIndex, offsetBy: i)]
+              d = [:]
+              start = i + 1
+            }
+        }
+        return ans
+    }
+}
+
+print(Solution().removeOuterParentheses("(()())(())(()(())))"))
